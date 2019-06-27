4ec3443a5806cff4d19af33951b48ecdceefce0d
Ojasvi1999 <ojasvigupta1234@gmail.com>
2019-06-27 13:44:15 +0530

    menu created

--- src/Schoolmanagement.java
+++ src/Schoolmanagement.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 import model.Student;
 import model.Teacher;
 public class Schoolmanagement {
@@ -5,6 +6,9 @@ public class Schoolmanagement {
 
 
         public static void main(String[] args){
+            Schoolmanagement sm = new Schoolmanagement();
+            sm.menu();
+
 
 
 
@@ -19,11 +23,53 @@ public class Schoolmanagement {
         }
 
         public void menu(){
-            System.out.println("MENU");
-            System.out.println("1. Add Student");
-            System.out.println("2. Remove Student");
-            System.out.println("3. Update Student");
-            System.out.println("Enter your choice: ");
+            int a=0,b,c;
+            while(a==0){
+            Scanner scan= new Scanner(System.in);
+
+            System.out.println("Press 1 for Menu");
+
+                b= scan.nextInt();
+
+                if(b==1)    {
+                    System.out.println("MENU");
+                    System.out.println("1. Add Student");
+                    System.out.println("2. Remove Student");
+                    System.out.println("3. Update Student");
+                    System.out.println("Enter your choice: ");
+
+                    c= scan.nextInt();
+
+                    switch(c){
+                        case 1:
+                            System.out.println("Adding Student");
+                            break;
+                        case 2:
+                            System.out.println("Removing Student");
+                            break;
+                        case 3:
+                            System.out.println("Updating Student");
+                            break;
+                        default:
+                            System.out.println("Wrong Selection");
+                            break;
+                    }
+
+
+                }
+                else{
+                    System.out.println("You Have Exited The Program");
+                    break;
+                }
+            }
         }
-    }
+}
+
+
+
+
+
+
+
+
 
