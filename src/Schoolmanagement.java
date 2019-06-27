51fcaeb2d2c336f003e71ed1bc23f4af92ca3731
divyan199 <tiwaridivyan7@gmail.com>
2019-06-27 10:52:04 +0530

    menu added

--- src/Schoolmanagement.java
+++ src/Schoolmanagement.java
@@ -1,29 +1,72 @@
 import model.Student;
 import model.Teacher;
+
+import java.util.Scanner;
+
 public class Schoolmanagement {
 
+    int first;
+
+    public static void main(String[] args) {
 
 
-        public static void main(String[] args){
+        //instantiation
+        Student student = new Student("Akshit", 2);
+        Teacher teacher = new Teacher();
+        teacher.setName("gg");
+
+
+        System.out.println(teacher.getName());
+        System.out.println(student.getRollNo());
 
 
 
-            //instantiation
-            Student student = new Student("Akshit", 2);
-            Teacher teacher=new Teacher();
-            teacher.setName("gg");
+        Schoolmanagement sm = new Schoolmanagement();
+        sm.menu();
 
 
-            System.out.println(teacher.getName());
-            System.out.println(student.getRollNo());
-        }
+    }
 
-        public void menu(){
-            System.out.println("MENU");
-            System.out.println("1. Add Student");
-            System.out.println("2. Remove Student");
-            System.out.println("3. Update Student");
-            System.out.println("Enter your choice: ");
+    public void menu() {
+        int loops = 0;
+        while (loops == 0) {
+
+            Scanner sca = new Scanner(System.in);
+            int a;
+            System.out.println("Press 1 to continue 0 to exit");
+            a = sca.nextInt();
+            if (a == 1) {
+                System.out.println("MENU");
+                System.out.println("1. Add Student");
+                System.out.println("2. Remove Student");
+                System.out.println("3. Update Student");
+                System.out.println("Enter your choice: ");
+                Scanner s=new Scanner(System.in);
+                int first;
+                System.out.println("Enter your choice");
+                first= s.nextInt();
+
+                switch (first) {
+
+                    case 1:
+                        System.out.println("Adding Student");
+                        break;
+                    case 2:
+                        System.out.println("Removing Student");
+                        break;
+                    case 3:
+                        System.out.println("Updating Student");
+                        break;
+                    default:
+                        System.out.println("Invalid choice");
+                        break;
+
+                }
+            } else {
+                break;
+            }
         }
     }
 
+
+}
